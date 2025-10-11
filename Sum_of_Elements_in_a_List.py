326be4598072a57ed86862d460502e6999b5837c
madhav-vashishtha <madhavvash1105@gmail.com>
2025-10-12 01:54:17 +0530

    sum of element in a list program

---
+++ Sum_of_Elements_in_a_List.py
@@ -0,0 +1,9 @@
+def sum_of_list(lst):
+    total = 0
+    for num in lst:
+        total += num
+    return total
+
+print(sum_of_list([7]))
+print(sum_of_list([-1, -2, -3, -4]))
+print(sum_of_list([1, 2, 3, 4, 5]))
