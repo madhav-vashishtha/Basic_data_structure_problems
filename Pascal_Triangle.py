23b32f6a572fa79b9ef617a3a608b48c907405e9
madhav-vashishtha <madhavvash1105@gmail.com>
2025-10-15 23:58:58 +0530

    pascal triangle program

---
+++ Pascal_Triangle.py
@@ -0,0 +1,18 @@
+def generate(numRows):
+    triangle = []  
+    
+    for i in range(numRows):
+        row = [1] * (i + 1)
+
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+        triangle.append(row)
+    
+    return triangle
+triangle = generate(5)
+
+print("[")
+for row in triangle:
+    print(" ", row, ",")
+print("]")
