6a07c7c76c5784eb40ac6547cd0f93f378b08026
madhav-vashishtha <madhavvash1105@gmail.com>
2025-12-25 21:11:41 +0530

    Leetcode Problem

---
+++ Perfect_Number.py
@@ -0,0 +1,16 @@
+def checkPerfectNumber(num):
+        if num <= 1:
+            return False
+
+        divisors_sum = 1
+
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                divisors_sum += i
+                if i != num // i:
+                    divisors_sum += num // i
+        
+        return divisors_sum == num
+
+print(checkPerfectNumber(28))
+print(checkPerfectNumber(45))
