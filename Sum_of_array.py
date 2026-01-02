77a529db47f971a0ed1d162914b246075e8a0be4
madhav-vashishtha <madhavvash1105@gmail.com>
2026-01-02 20:59:26 +0530

    Simple Problem

---
+++ Sum_of_array.py
@@ -0,0 +1,12 @@
+def Sum_of_array(arr):
+    total = 0
+    
+    for element in arr:
+        total += element
+
+    return total
+
+array = [1, 2, 3]
+result = Sum_of_array(array)
+print("Sum of the array:", result)
+
